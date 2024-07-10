e5b21d0ad1f23b290ac325829c87213c555db97f
Axel <augisax@gmail.com>
2024-07-10 15:58:15 +0200

    first commit

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -1,1 +1,42 @@
 <?php
+
+// src/Controller/MailerController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Part\DataPart;
+use Symfony\Component\Mime\Part\File;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MailerController extends AbstractController
+{
+    #[Route('/email')]
+    public function sendEmail(MailerInterface $mailer): JsonResponse
+    {
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to('you@example.com')
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>')
+            ->addPart(new DataPart(new File('pdf/user_1.pdf')));
+
+       try {
+           $mailer->send($email);
+           return new JsonResponse([
+               'success' => true,
+               'message' => 'Email sent!'
+           ]);
+       } catch (TransportExceptionInterface) {
+           return new JsonResponse(['error' => 'TransportExceptionInterface'], Response::HTTP_INTERNAL_SERVER_ERROR);
+       }
+
+
+    }
+}
+
--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -1,1 +1,41 @@
 <?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Snappy\Pdf;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class PdfController extends AbstractController
+{
+    private $knpSnappyPdf;
+    private $entityManager;
+
+    public function __construct(Pdf $knpSnappyPdf, EntityManagerInterface $entityManager)
+    {
+        $this->knpSnappyPdf = $knpSnappyPdf;
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/generate-pdf-user/{id}', name: 'app_generate_pdf_user')]
+    public function generatePdf($id): Response
+    {
+        $user = $this->entityManager->getRepository(User::class)->find($id);
+        $html = $this->renderView('pdf/user.html.twig', [
+            'user' => $user
+        ]);
+
+        $publicDirectory = $this->getParameter('kernel.project_dir') . '/public';
+        $pdfFilePath = $publicDirectory . '/pdf/user_' . $user->getId() . '.pdf';
+
+        $this->knpSnappyPdf->generateFromHtml($html, $pdfFilePath);
+
+        return new JsonResponse([
+            'message' => 'PDF généré avec succès',
+            'file_path' => '/pdf/user_' . $user->getId() . '.pdf'
+        ]);
+    }
+}
